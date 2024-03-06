7702cf41a4782d5828ad7e923e41eb80ebb9a593
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2024-03-06 10:16:38 +0000

    feat: add new code engine module

--- main.tf
+++ main.tf
@@ -1,3 +1,131 @@
-/********************************************************************
-This file is used to implement the ROOT module.
-*********************************************************************/
+
+locals {
+
+  apps = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for app in(var.code_engine[project.name].apps != null ? var.code_engine[project.name].apps : []) : merge(app, { project_id = project.id })
+    ]
+  ])
+  jobs = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for job in(var.code_engine[project.name].jobs != null ? var.code_engine[project.name].jobs : []) : merge(job, { project_id = project.id })
+    ]
+  ])
+  config_maps = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for config_map in(var.code_engine[project.name].config_maps != null ? var.code_engine[project.name].config_maps : []) : merge(config_map, { project_id = project.id })
+    ]
+  ])
+  secrets = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for secret in(var.code_engine[project.name].secrets != null ? var.code_engine[project.name].secrets : []) : merge(secret, { project_id = project.id })
+    ]
+  ])
+  builds = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for build in(var.code_engine[project.name].builds != null ? var.code_engine[project.name].builds : []) : merge(build, { project_id = project.id })
+    ]
+  ])
+  bindings = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for binding in(var.code_engine[project.name].bindings != null ? var.code_engine[project.name].bindings : []) : merge(binding, { project_id = project.id })
+    ]
+  ])
+  domain_mappings = flatten([
+    for project in resource.ibm_code_engine_project.ce_project : [
+      for domain_mapping in(var.code_engine[project.name].domain_mappings != null ? var.code_engine[project.name].domain_mappings : []) : merge(domain_mapping, { project_id = project.id })
+    ]
+  ])
+}
+
+##############################################################################
+# Code Engine Project
+##############################################################################
+resource "ibm_code_engine_project" "ce_project" {
+  for_each          = var.code_engine
+  name              = each.key
+  resource_group_id = var.resource_group_id
+}
+
+resource "ibm_code_engine_app" "ce_app" {
+  count           = length(local.apps)
+  project_id      = local.apps[count.index].project_id
+  name            = local.apps[count.index].name
+  image_reference = local.apps[count.index].image_reference
+
+  run_env_variables {
+    type  = "literal"
+    name  = "name"
+    value = "value"
+  }
+}
+
+resource "ibm_code_engine_job" "ce_job" {
+  count           = length(local.jobs)
+  project_id      = local.jobs[count.index].project_id
+  name            = local.jobs[count.index].name
+  image_reference = local.jobs[count.index].image_reference
+
+  run_env_variables {
+    type  = "literal"
+    name  = "name"
+    value = "value"
+  }
+}
+
+resource "ibm_code_engine_config_map" "ce_config_map" {
+  count      = length(local.config_maps)
+  project_id = local.config_maps[count.index].project_id
+  name       = local.config_maps[count.index].name
+  data = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+}
+
+resource "ibm_code_engine_secret" "code_engine_secret_instance" {
+  count      = length(local.secrets)
+  project_id = local.secrets[count.index].project_id
+  name       = local.secrets[count.index].name
+  format     = local.secrets[count.index].format
+
+  data = {
+    key1 = "value1"
+    key2 = "value2"
+  }
+}
+
+resource "ibm_code_engine_build" "ce_build" {
+  count         = length(local.builds)
+  project_id    = local.builds[count.index].project_id
+  name          = local.builds[count.index].name
+  output_image  = local.builds[count.index].output_image
+  output_secret = local.builds[count.index].output_secret
+  source_url    = local.builds[count.index].source_url
+  strategy_type = local.builds[count.index].strategy_type
+}
+
+# resource "ibm_code_engine_domain_mapping" "ce_domain_mapping" {
+#     count         = length(local.domain_mappings)
+#     project_id    = local.domain_mappings[count.index].project_id
+#     name          = local.domain_mappings[count.index].name
+#     tls_secret          = local.domain_mappings[count.index].tls_secret
+#   component {
+#     for_each = local.domain_mappings[count.index].component
+#         name = each.value.name
+#         resource_type = each.value.resource_type
+#   }
+
+# }
+
+resource "ibm_code_engine_binding" "ce_binding" {
+  count       = length(local.bindings)
+  project_id  = local.bindings[count.index].project_id
+  prefix      = local.bindings[count.index].prefix
+  secret_name = local.bindings[count.index].secret_name
+  component {
+    name          = "my-app-1"
+    resource_type = "app_v2"
+  }
+
+}
--- variables.tf
+++ variables.tf
@@ -2,8 +2,109 @@
 # Input Variables
 ########################################################################################################################
 
-#variable "my_variable" {
-#  type        = string
-#  description = "A description of my variable"
-#  default     = "default_value"
-#}
+variable "resource_group_id" {
+  description = "ID of resource group to use when creating the VPC and PAG"
+  type        = string
+}
+
+
+########################################################################################################################
+# Code Engine
+########################################################################################################################
+
+variable "code_engine" {
+  description = ""
+  type = map(object({
+    apps = optional(list(object({
+      name            = string
+      image_reference = string
+    }))),
+    jobs = optional(list(object({
+      name            = string
+      image_reference = string
+    }))),
+    config_maps = optional(list(object({
+      name = string
+    }))),
+    secrets = optional(list(object({
+      name   = string
+      format = string
+    }))),
+    builds = optional(list(object({
+      name          = string
+      output_image  = string
+      output_secret = string
+      source_url    = string
+      strategy_type = string
+    })))
+    bindings = optional(list(object({
+      prefix      = string
+      secret_name = string
+      component = list(object({
+        name          = string
+        resource_type = string
+      }))
+    })))
+    domain_mappings = optional(list(object({
+      name = string
+      component = list(object({
+        name          = string
+        resource_type = string
+      }))
+      tls_secret = string
+    })))
+
+  }))
+  default = {
+    "project_11" = {
+      apps = [{
+        name            = "app-name1"
+        image_reference = "icr.io/codeengine/helloworld"
+        },
+        {
+          name            = "app-name2"
+          image_reference = "icr.io/codeengine/helloworld"
+      }],
+      jobs = [{
+        name            = "jobs-1"
+        image_reference = "icr.io/codeengine/helloworld"
+      }],
+      config_maps = [{
+        name = "config-map-1"
+      }],
+      secrets = [{
+        name   = "secret-1"
+        format = "generic"
+      }],
+      builds = [{
+        name          = "build-1"
+        output_image  = "private.de.icr.io/icr_namespace/image-name"
+        output_secret = "ce-auto-icr-private-eu-de"
+        source_url    = "https://github.com/IBM/CodeEngine"
+        strategy_type = "dockerfile"
+      }],
+      domain_mappings = [{
+        name = "domain_mapping-1"
+        component = [{
+          name          = "component-name"
+          resource_type = "app_v2"
+        }]
+        tls_secret = "my-tls-secret"
+      }],
+      bindings = [{
+        prefix = "BINDING_1"
+        component = [{
+          name          = "component-name"
+          resource_type = "app_v2"
+        }]
+        secret_name = "my-service-access"
+      }],
+    },
+    "project_22" = {
+      apps = [{
+        name            = "app-name1"
+        image_reference = "icr.io/codeengine/helloworld"
+      }]
+    }
+  }
+}
