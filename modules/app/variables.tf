2693bd84cc7259455e0a5611003577f251b7f830
Akash Kumar <Ak-sky@users.noreply.github.com>
2024-12-13 02:11:56 +0530

    fix: added validation for the `managed_domain_mappings` input (#138)

--- modules/app/variables.tf
+++ modules/app/variables.tf
@@ -54,9 +54,13 @@ variable "image_port" {
 }
 
 variable "managed_domain_mappings" {
-  description = "Define which of the system managed domain mappings will be setup for the application. Valid values are 'local_public', 'local_private' and 'local'."
+  description = "Define which of the following values for the system-managed domain mappings to set up for the application: `local_public`, `local_private`, and `local`. See https://cloud.ibm.com/docs/codeengine?topic=codeengine-application-workloads#optionsvisibility"
   type        = string
   default     = null
+  validation {
+    condition     = var.managed_domain_mappings == null || can(regex("local_public|local_private|local", var.managed_domain_mappings))
+    error_message = "Valid values are 'local_public', 'local_private', or 'local'."
+  }
 }
 
 variable "run_arguments" {
