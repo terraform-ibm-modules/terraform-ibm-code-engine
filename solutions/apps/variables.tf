2dc53c691a1c0e04231625d9311075b1ba5c8127
Arya Girish K <arya.girish.k@ibm.com>
2025-03-03 14:24:18 +0530

    Added prefix logic in apps variant

--- solutions/apps/variables.tf
+++ solutions/apps/variables.tf
@@ -25,6 +25,13 @@ variable "region" {
   default     = "us-south"
 }
 
+variable "prefix" {
+  type        = string
+  description = "Prefix added to the project created by this solution. To not use any prefix value, you can set this value to `null` or an empty string."
+  default     = "dev"
+  nullable    = true
+}
+
 variable "existing_resource_group" {
   type        = bool
   description = "Whether to use an existing resource group."
@@ -39,7 +46,6 @@ variable "resource_group_name" {
 variable "project_name" {
   description = "The name of the project to add the IBM Cloud Code Engine resources to. If the value of `var.existing_project_id` is `null`, the project name is required."
   type        = string
-  default     = null
 }
 
 variable "existing_project_id" {
