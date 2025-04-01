8eb0c4dd73d728d6d23e296d8b417ef783ed5023
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2025-04-01 11:43:41 +0100

    private visibility

--- examples/apps/main.tf
+++ examples/apps/main.tf
@@ -181,7 +181,7 @@ module "code_engine" {
           attributes = [
             {
               "name" : "endpointType",
-              "value" : "public-and-private"
+              "value" : "private"
             },
             {
               name  = "networkZoneId"
