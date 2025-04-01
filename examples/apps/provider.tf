6ac4d015b277491d8b6824108df3dbfcf4b7c178
akocbek@ie.ibm.com <akocbek@ie.ibm.com>
2025-04-01 13:33:48 +0100

    do not expose provider visibility

--- examples/apps/provider.tf
+++ examples/apps/provider.tf
@@ -5,12 +5,12 @@
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.region
-  visibility       = var.provider_visibility
+  visibility       = "private"
 }
 
 provider "ibm" {
   ibmcloud_api_key = var.ibmcloud_api_key
   region           = var.existing_sm_instance_region == null ? var.region : var.existing_sm_instance_region
   alias            = "ibm-sm"
-  visibility       = var.provider_visibility
+  visibility       = "private"
 }
--- examples/apps/variables.tf
+++ examples/apps/variables.tf
@@ -109,14 +109,3 @@ variable "acme_letsencrypt_private_key" {
   default     = null
   sensitive   = true
 }
-
-variable "provider_visibility" {
-  description = "Set the visibility value for the IBM terraform provider. Supported values are `public`, `private`, `public-and-private`. [Learn more](https://registry.terraform.io/providers/IBM-Cloud/ibm/latest/docs/guides/custom-service-endpoints)."
-  type        = string
-  default     = "private"
-
-  validation {
-    condition     = contains(["public", "private", "public-and-private"], var.provider_visibility)
-    error_message = "Invalid visibility option. Allowed values are 'public', 'private', or 'public-and-private'."
-  }
-}
