ffe51984482f9f086e1d466d6b38a65a0aef2ca7
akocbek <106765658+akocbek@users.noreply.github.com>
2025-04-07 15:28:48 +0100

    feat: Support has been removed from the DA for creating a new resource group<br>- `existing_resource_group` and `resource_group_name` inputs have been replaced by new input `existing_resource_group_name` (with a default value of `Default`) (#177)
    BREAKING CHANGE: If you are upgrading from a previous release and creating a new resource group as part of the DA, resources will be recreated if you are not using the Default resource group

--- solutions/apps/main.tf
+++ solutions/apps/main.tf
@@ -5,8 +5,7 @@
 module "resource_group" {
   source                       = "terraform-ibm-modules/resource-group/ibm"
   version                      = "1.2.0"
-  resource_group_name          = var.existing_resource_group == false ? var.resource_group_name : null
-  existing_resource_group_name = var.existing_resource_group == true ? var.resource_group_name : null
+  existing_resource_group_name = var.existing_resource_group_name
 }
 
 locals {
