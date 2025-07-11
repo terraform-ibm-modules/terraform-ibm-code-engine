007117ca56de5479c57d9080b57d003bf71f26b8
akocbek <akocbek>
2025-07-11 12:29:14 +0100

    update das

--- solutions/app-from-binary/outputs.tf
+++ solutions/app-from-binary/outputs.tf
@@ -2,32 +2,32 @@
 # Outputs
 ########################################################################################################################
 
-# output "project_id" {
-#   description = "ID of the created code engine project."
-#   value       = module.code_engine.project_id
-# }
+output "project_id" {
+  description = "ID of the created code engine project."
+  value       = module.code_engine.project_id
+}
 
-# output "app" {
-#   description = "Configuration of the created code engine app."
-#   value       = module.code_engine.app
-# }
+output "app" {
+  description = "Configuration of the created code engine app."
+  value       = module.code_engine.app
+}
 
-# output "config_map" {
-#   description = "Configuration of the created code engine config map."
-#   value       = module.code_engine.config_map
-# }
+output "config_map" {
+  description = "Configuration of the created code engine config map."
+  value       = module.code_engine.config_map
+}
 
-# output "secret" {
-#   description = "Configuration of the created code engine secret."
-#   value       = module.code_engine.secret
-# }
+output "secret" {
+  description = "Configuration of the created code engine secret."
+  value       = module.code_engine.secret
+}
 
-# output "domain_mapping" {
-#   description = "Configuration of the created code engine domain maping."
-#   value       = module.code_engine.domain_mapping
-# }
+output "domain_mapping" {
+  description = "Configuration of the created code engine domain maping."
+  value       = module.code_engine.domain_mapping
+}
 
-# output "binding" {
-#   description = "Configuration of the created code engine binding."
-#   value       = module.code_engine.binding
-# }
+output "binding" {
+  description = "Configuration of the created code engine binding."
+  value       = module.code_engine.binding
+}
