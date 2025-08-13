94884d30481d01803f8929e6628f65ba2ce2d23f
akocbek <akocbek>
2025-08-13 12:49:35 +0100

    address rally comments

--- solutions/app-from-source/main.tf
+++ solutions/app-from-source/main.tf
@@ -1,7 +1,7 @@
 locals {
   prefix       = var.prefix != null ? trimspace(var.prefix) != "" ? "${var.prefix}-" : "" : ""
   project_name = "${local.prefix}${var.project_name}"
-  app_name     = "${local.prefix}${var.app.name}"
+  app_name     = "${local.prefix}${var.app_name}"
 }
 
 ########################################################################################################################
@@ -39,26 +39,28 @@ locals {
   fail_if_registry_region_error = local.registry_region_error != null ? tobool("Registry region script failed: ${local.registry_region_error}") : null
 
   # if no build defines a container image reference (output_image), a new container registry namespace must be created using container_registry_namespace.
-  any_missing_output_image = anytrue([
-    for build in values(var.builds) :
-    !contains(keys(build), "output_image") || build.output_image == null
-  ])
-  image_container = local.any_missing_output_image && local.container_registry != null ? "${local.container_registry}/${resource.ibm_cr_namespace.my_namespace[0].name}" : ""
+  # any_missing_output_image = anytrue([
+  #   for build in values(var.builds) :
+  #   !contains(keys(build), "output_image") || build.output_image == null
+  # ])
 
+  image_container = var.output_image == null && local.container_registry != null ? "${local.container_registry}/${resource.ibm_cr_namespace.my_namespace[0].name}" : ""
+  output_image    = var.output_image != null ? var.output_image : "${local.image_container}/${var.build_name}"
   # if output_image not exists then a new created container image reference
-  updated_builds = {
-    for name, build in var.builds :
-    name => merge(
-      build,
-      {
-        output_image = coalesce(build.output_image, "${local.image_container}/${name}")
-      }
-    )
-  }
+  # updated_builds = {
+  #   for name, build in var.builds :
+  #   name => merge(
+  #     build,
+  #     {
+  #       output_image = coalesce(build.output_image, "${local.image_container}/${name}")
+  #     }
+  #   )
+  # }
+
 }
 
 resource "ibm_cr_namespace" "my_namespace" {
-  count = local.any_missing_output_image && var.container_registry_namespace != null ? 1 : 0
+  count = var.output_image == null && var.container_registry_namespace != null ? 1 : 0
   name  = var.container_registry_namespace
 }
 
@@ -75,22 +77,21 @@ data "external" "container_registry_region" {
 module "build" {
   depends_on                 = [module.secret]
   source                     = "../../modules/build"
-  for_each                   = local.updated_builds
-  project_id                 = module.project.project_id
-  name                       = each.key
-  output_image               = each.value.output_image
-  output_secret              = each.value.output_secret != null ? each.value.output_secret : local.registry_name
-  source_url                 = each.value.source_url
-  strategy_type              = each.value.strategy_type
-  source_context_dir         = each.value.source_context_dir
-  source_revision            = each.value.source_revision
-  source_secret              = each.value.source_secret
-  source_type                = each.value.source_type
-  strategy_size              = each.value.strategy_size
-  strategy_spec_file         = each.value.strategy_spec_file
-  timeout                    = each.value.timeout
-  region                     = var.region
   ibmcloud_api_key           = var.ibmcloud_api_key
+  project_id                 = module.project.project_id
+  name                       = var.build_name
+  output_image               = local.output_image
+  output_secret              = var.output_secret != null ? var.output_secret : local.registry_secret_name
+  source_url                 = var.source_url
+  strategy_type              = var.strategy_type
+  source_context_dir         = var.source_context_dir
+  source_revision            = var.source_revision
+  source_secret              = var.source_secret
+  source_type                = var.source_type
+  strategy_size              = var.strategy_size
+  strategy_spec_file         = var.strategy_spec_file
+  timeout                    = var.timeout
+  region                     = var.region
   existing_resource_group_id = module.resource_group.resource_group_id
 }
 
@@ -138,12 +139,12 @@ locals {
   ]
   has_registry = length(local.registry_secret_names) > 0
 
-  registry_name = local.has_registry ? local.registry_secret_names[0] : "${local.prefix}registry"
+  registry_secret_name = local.has_registry ? local.registry_secret_names[0] : "${local.prefix}registry"
 
   secrets = local.has_registry ? var.secrets : merge(
     var.secrets,
     {
-      (local.registry_name) = {
+      (local.registry_secret_name) = {
         format = "registry"
         data = {
           password = var.ibmcloud_api_key,
@@ -170,34 +171,35 @@ module "secret" {
 # Code Engine Apps
 ##############################################################################
 locals {
-  image_reference = length(var.builds) > 0 ? module.build[keys(var.builds)[0]].output_image : null
+  image_reference = var.app_image_reference != null ? var.app_image_reference : module.build.output_image
 }
 
 module "app" {
-  source                        = "../../modules/app"
-  count                         = var.app == null ? 0 : 1
-  image_port                    = var.app.image_port
-  image_reference               = var.app.image_reference != null ? var.app.image_reference : local.image_reference
-  image_secret                  = var.app.image_secret != null ? var.app.image_secret : local.registry_name
-  managed_domain_mappings       = var.app.managed_domain_mappings
-  name                          = local.app_name
-  project_id                    = module.project.project_id
-  run_arguments                 = var.app.run_arguments
-  run_as_user                   = var.app.run_as_user
-  run_commands                  = var.app.run_commands
-  run_env_variables             = var.app.run_env_variables
-  run_service_account           = var.app.run_service_account
-  run_volume_mounts             = var.app.run_volume_mounts
-  scale_concurrency             = var.app.scale_concurrency
-  scale_concurrency_target      = var.app.scale_concurrency_target
-  scale_cpu_limit               = var.app.scale_cpu_limit
-  scale_down_delay              = var.app.scale_down_delay
-  scale_ephemeral_storage_limit = var.app.scale_ephemeral_storage_limit
-  scale_initial_instances       = var.app.scale_initial_instances
-  scale_max_instances           = var.app.scale_max_instances
-  scale_memory_limit            = var.app.scale_memory_limit
-  scale_min_instances           = var.app.scale_min_instances
-  scale_request_timeout         = var.app.scale_request_timeout
+  source = "../../modules/app"
+  # count                         = var.app == null ? 0 : 1
+  name            = local.app_name
+  image_reference = local.image_reference
+  image_secret    = var.app_image_secret != null ? var.app_image_secret : local.registry_secret_name
+  project_id      = module.project.project_id
+
+  image_port = var.app_image_port
+  # run_arguments       = var.run_arguments
+  # run_as_user         = var.run_as_user
+  # run_commands        = var.run_commands
+  # run_env_variables   = var.run_env_variables
+  # run_service_account = var.run_service_account
+  # run_volume_mounts   = var.run_volume_mounts
+  managed_domain_mappings = var.managed_domain_mappings
+  # scale_concurrency             = var.app.scale_concurrency
+  # scale_concurrency_target      = var.app.scale_concurrency_target
+  scale_cpu_limit = var.app_scale_cpu_limit
+  # scale_down_delay              = var.app.scale_down_delay
+  scale_ephemeral_storage_limit = var.app_scale_ephemeral_storage_limit
+  # scale_initial_instances       = var.app.scale_initial_instances
+  # scale_max_instances           = var.app.scale_max_instances
+  scale_memory_limit = var.app_scale_memory_limit
+  # scale_min_instances           = var.app.scale_min_instances
+  # scale_request_timeout         = var.app.scale_request_timeout
 }
 
 ##############################################################################
