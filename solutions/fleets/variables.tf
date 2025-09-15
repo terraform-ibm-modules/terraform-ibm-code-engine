137f27b66637ec8d501f153b5d2372b1da44a522
akocbek <akocbek>
2025-09-15 21:36:21 +0100

    vpc DA

--- solutions/fleets/variables.tf
+++ solutions/fleets/variables.tf
@@ -228,7 +228,7 @@ variable "ex_subnet_zone_list" {
   })
 
   validation {
-    condition     = alltrue([for key, value in var.subnets : value != null ? length([for subnet in value : subnet.public_gateway if subnet.public_gateway]) > 1 ? false : true : true])
+    condition     = alltrue([for key, value in var.ex_subnet_zone_list : value != null ? length([for subnet in value : subnet.public_gateway if subnet.public_gateway]) > 1 ? false : true : true])
     error_message = "var.subnets has more than one public gateway in a zone. Only one public gateway can be attached to a zone for the virtual private cloud."
   }
 }
