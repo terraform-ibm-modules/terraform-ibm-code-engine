be9797c82084e945f4371c2638435b707d53fdd3
akocbek <akocbek>
2025-10-16 13:38:09 +0100

    feat: add support to create container registry and secret inside build module

---
+++ examples/build/main.tf
@@ -0,0 +1,30 @@
+########################################################################################################################
+# Resource group
+########################################################################################################################
+
+module "resource_group" {
+  source  = "terraform-ibm-modules/resource-group/ibm"
+  version = "1.3.0"
+  # if an existing resource group is not set (null) create a new one using prefix
+  resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
+  existing_resource_group_name = var.resource_group
+}
+
+########################################################################################################################
+# Code Engine instance
+########################################################################################################################
+
+module "code_engine" {
+  source            = "../.."
+  ibmcloud_api_key  = var.ibmcloud_api_key
+  resource_group_id = module.resource_group.resource_group_id
+  project_name      = "${var.prefix}-project"
+  builds = {
+    "${var.prefix}-build1" = {
+      source_url                   = "https://github.com/IBM/CodeEngine"
+      container_registry_namespace = "cr-ce"
+      prefix                       = var.prefix
+      region                       = var.region
+    }
+  }
+}
