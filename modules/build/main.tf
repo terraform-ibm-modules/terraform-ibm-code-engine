d108ec92636946197a1cf58ea52325584b906232
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-11-29 12:17:46 +0000

    fix(deps): update dependencies (#271)

--- modules/build/main.tf
+++ modules/build/main.tf
@@ -61,14 +61,14 @@ locals {
 module "cr_namespace" {
   count             = local.create_cr_namespace ? 1 : 0
   source            = "terraform-ibm-modules/container-registry/ibm"
-  version           = "2.1.0"
+  version           = "2.3.5"
   namespace_name    = "${local.prefix}${var.container_registry_namespace}"
   resource_group_id = var.existing_resource_group_id
 }
 
 module "cr_endpoint" {
   source  = "terraform-ibm-modules/container-registry/ibm//modules/endpoint"
-  version = "2.1.0"
+  version = "2.3.5"
   region  = var.region
 }
 
