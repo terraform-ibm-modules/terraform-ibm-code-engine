c5d852866a0f0c4e56b34adf6f095f7e158e195c
Harika <86517031+HarikaPonna@users.noreply.github.com>
2026-03-02 16:08:04 +0530

    feat: added support to install `jq`, the `ibmcloud` CLI, and the `code-engine` CLI plugin if they do not already exist in the runtime. They will be downloaded to the /tmp directory. This requires public internet access to download. This can be disabled by setting `install_required_binaries` to false. (#312)

--- modules/build/variables.tf
+++ modules/build/variables.tf
@@ -149,3 +149,10 @@ variable "container_registry_api_key" {
   sensitive   = true
   default     = null
 }
+
+variable "install_required_binaries" {
+  type        = bool
+  default     = true
+  description = "When set to true, a script will run to check if `jq`, the `ibmcloud` CLI, and the `code-engine` plugin exist on the runtime and if not attempt to download them from the public internet and install them to /tmp. Set to false to skip running this script."
+  nullable    = false
+}
